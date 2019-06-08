4221cbea44bf93bcbab2c098a0f9096309e671e8
damp0010 <end.of.the.road.10@gmail.com>
2019-06-08 10:58:30 +0900

    Initial Commit

---
+++ MarubatuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarubatuApp
+//
+//  Created by 田中　徳充 on 2019/06/08.
+//  Copyright © 2019 田中　徳充. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
