1c46beafc846615c1ef7c8b613c734514dac4f7f
damp0010 <end.of.the.road.10@gmail.com>
2019-06-10 14:58:16 +0900

    marubatuApp作成完了

---
+++ MarubatuApp/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  MarubatuApp
+//
+//  Created by 田中　徳充 on 2019/06/10.
+//  Copyright © 2019 田中　徳充. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
