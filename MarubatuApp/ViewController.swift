8b57c17a56fb272103dbbb314297df87d6ad347f
damp0010 <end.of.the.road.10@gmail.com>
2019-06-10 15:05:45 +0900

    marubatuApp完成

--- MarubatuApp/ViewController.swift
+++ MarubatuApp/ViewController.swift
@@ -13,8 +13,83 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    showQuestion()
+    
     }
 
+    @IBOutlet weak var TextLabel: UILabel!
+    var currentQuestionNum:Int = 0
+    //    String:Any にすることで数字もOK
+//    arrey & dictionary
+    let  questions:[[String:Any]] = [
+        [
+            "question": "スパルタキャンプは体を鍛える合宿である",
+            "answer": false
+        ],
+        [
+            "question": "スパルタキャンプの授業料は無料である",
+            "answer": true
+        ],
+        [
+            "question": "スパルタキャンプの宿泊は無料である",
+            "answer": true
+        ]
+    
+    
+    
+    ]
+    
+    
+    func showQuestion() {
+        let question = questions[currentQuestionNum]
+        
+        if let que = question["question"] as? String{
+            TextLabel.text = que
+        }
+        
+    }
+    func checkAnswer(yourAnswer: Bool){
+        let question = questions[currentQuestionNum]
+        if let ans = question["answer"] as? Bool{
+            if yourAnswer == ans{
+//            正解(true)
+                showAlert(message: "正解！")
+                currentQuestionNum += 1
+            }else{
+//                不正解(false)
+                showAlert(message: "不正解…")
+            }
+        }else{
+        
+        return
+        }
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
+        
+    showQuestion()
+    }
 
+    func showAlert(message: String) {
+    let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+    let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+    alert.addAction(close)
+    present(alert, animated: true, completion: nil)
+    
+    }
+    
+    
+    
+    
+    @IBAction func maruButton(_ sender: Any) {
+        checkAnswer(yourAnswer: true)
+        
+    }
+    
+    @IBAction func batuButton(_ sender: Any) {
+    checkAnswer(yourAnswer: false)
+    }
+    
+    
 }
 
